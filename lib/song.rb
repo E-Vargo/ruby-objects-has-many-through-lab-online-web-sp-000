3cf08583753d1d78c44da3836c5cc6b54bd47127
Elliott Vargo <vargoelliott@gmail.com>
2021-04-10 20:05:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,14 @@
 class Song
 
-attr_accessor :title, :genre, :artist
+attr_accessor :name, :artist, :genre
 
 @@all = []
 
-def initialize(title)
+def initialize(name, artist, genre)
+  @name = name 
+  @artist = artist
+  @genre = genre 
+  @@all << self
 end
 
 def self.all
