564bcd3007b2a509783cb25ac6b11fb6a081d6ca
Elliott Vargo <vargoelliott@gmail.com>
2021-04-10 23:54:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,10 +13,10 @@ def self.all
   @@all
 end
 
-def new_song(name, artist, genre)
-  name  = Song.new
-  name.artist = self
+def new_song(name, genre)
+Song.new(name, self, genre)
 end
+
 def songs
   Song.all.select {|song| song.artist == self}
 end
